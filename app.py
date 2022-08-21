6fdfe0fbdb19ece48269db2977e5d305c5023e94
ivan1384 <111645617+ivan1384@users.noreply.github.com>
2022-08-21 19:20:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
-from predict_alarm import predict_alarm
+from predict_alarm import show_alarm_page
 from explore_page import show_explore_page
 
 
 page = st.sidebar.selectbox("Explore Or Hotspot", ("Hotspot", "Explore"))
 
 if page == "Hotspot":
-    show_predict_page()
+    show_alarm_page()
 else:
     show_explore_page()
