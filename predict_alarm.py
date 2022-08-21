0c462762fdfa6814268d6a8f1cba906a3b6a7584
ivan1384 <111645617+ivan1384@users.noreply.github.com>
2022-08-21 19:30:16 +0800

    Update predict_alarm.py

--- predict_alarm.py
+++ predict_alarm.py
@@ -11,7 +11,7 @@ rf = pic["model"]
 
 
 def show_alarm_page():
-    st.title("Crime Alarm For Each police District")
+    st.title("Crime Alarm For each police District")
 
     st.write("""### We need some the district,time and date to predict the alarm""")
 
@@ -30,6 +30,7 @@ def show_alarm_page():
     Pdoptions = list(range(len(PdDistrict)))
     value = st.selectbox("PdDistrict", Pdoptions, format_func=lambda x: PdDistrict[x])
 
+    date= st.date_input("When's your birthday",datetime.date(2019, 7, 6))
     
     ok = st.button("Predict Alarm")
     if ok:
