a64ffb39000a9fa30b339e8e6280e0135cce1004
ivan1384 <111645617+ivan1384@users.noreply.github.com>
2022-08-21 21:23:15 +0800

    Update predict_alarm.py

--- predict_alarm.py
+++ predict_alarm.py
@@ -36,7 +36,7 @@ def show_alarm_page():
     
     ok = st.button("Predict Alarm")
     if ok:
-        scrimed = datetime.strptime(crimed, '%Y-%m-%d')
+        scrimed = datetime.strftime(crimed, '%Y-%m-%d')
         st.write(scrimed)
         datem = parse(scrimed)
         
