5d53bf17c1f92c8bf2874c47492d77f9e009695f
ivan1384 <111645617+ivan1384@users.noreply.github.com>
2022-08-22 15:44:33 +0800

    Update crimeclassifier.py

--- crimeclassifier.py
+++ crimeclassifier.py
@@ -43,9 +43,9 @@ def show_classifier_page():
     streetoptions = list(range(len(Street)))
     Streetvalue =st.selectbox("Street Type", streetoptions, format_func=lambda x: Street[x])
     
-    Longitude = st.number_input('Insert a number',max_value=-122.364937,min_value=-122.513642)
+    Longitude = st.number_input('Insert a Longitude',max_value=-122.364937,min_value=-122.513642)
     st.write('The current number is ', Longitude)
-    Latitude = st.number_input('Insert a number',max_value=37.8206208380702,min_value=37.7078790224135)
+    Latitude = st.number_input('Insert a Latitude',max_value=37.8206208380702,min_value=37.7078790224135)
     st.write('The current number is ', Latitude)
     
     
