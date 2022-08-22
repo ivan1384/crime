d41002d26585beabd476434c275a2904d697527d
ivan1384 <111645617+ivan1384@users.noreply.github.com>
2022-08-22 17:18:48 +0800

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -19,7 +19,7 @@ def show_explore_page():
     data.columns = ['Dates','Category','Descript','DayOfWeek','PdDistrict','Resolution','Address','longitude','latitude']
     
     st.sidebar.markdown('Type of crimes and their stats')
-    select = st.sidebar.selectbox('visualization type',['Histogram','Pie chart'],key=1)
+    select = st.sidebar.selectbox('visualization type',['Histogram','Pie chart'])
     crime_count = data['Category'].value_counts()
     crime_count = pd.DataFrame({'Crime':crime_count.index, 'Numbers': crime_count.values})
     if not st.sidebar.checkbox("Hide",True):
@@ -38,7 +38,7 @@ def show_explore_page():
     mod_dat = data[data['Dates'].dt.hour == hour]
     mod_dat = mod_dat[data['Dates'].dt.year == year]
     mod_dat = mod_dat[data['Dates'].dt.month == day]
-    if not st.sidebar.checkbox("close",True,key='1'):
+    if not st.sidebar.checkbox("close",True):
         st.markdown("Crime location based on time")
         st.markdown("%i crime between %i:00 and %i:00 in the Month of %i" % (len(mod_dat),hour,hour+1%24,day))
         st.map(mod_dat)
