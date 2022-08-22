524c4b2de7b47cc58be9783d27c07d9dd9550a54
ivan1384 <111645617+ivan1384@users.noreply.github.com>
2022-08-22 17:10:10 +0800

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -37,7 +37,7 @@ def show_explore_page():
     mod_dat = data[data['Dates'].dt.hour == hour]
     mod_dat = mod_dat[data['Dates'].dt.year == year]
     mod_dat = mod_dat[data['Dates'].dt.month == day]
-    if not st.sidebar.checkbox("close",True,key='1'):
+    if not st.sidebar.checkbox("close",True,key='2'):
         st.markdown("Crime location based on time")
         st.markdown("%i crime between %i:00 and %i:00 in the Month of %i" % (len(mod_dat),hour,hour+1%24,day))
         st.map(mod_dat)
