29865ffd15ca6196f71d070f977bbec6856b1500
ivan1384 <111645617+ivan1384@users.noreply.github.com>
2022-08-22 11:40:49 +0800

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -9,5 +9,10 @@ import folium
 def show_explore_page():
     Longitude = st.number_input('Insert a number',max_value=-122.364937,min_value=-122.513642)
     st.write('The current number is ', Longitude)
-    Latitude = st.number_input('Insert a number',max_value=37.819975,min_value=37.707879)
+    Latitude = st.number_input('Insert a number',max_value=37.8206208380702,min_value=37.7078790224135)
     st.write('The current number is ', Latitude)
+    cor = np.array([[Longitude,Latitude]])
+
+    
+    
+    st.map(cor)
