896b8fc8f17603bcf86b33ee3b4cd2b7374e525e
ivan1384 <111645617+ivan1384@users.noreply.github.com>
2022-08-22 11:54:13 +0800

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -14,6 +14,6 @@ def show_explore_page():
     
     ok = st.button("Show map")
     if ok:
-        cor = np.array([[Latitude,Longitude]])
+        pd.DataFrame(Latitude,Longitude,columns=['lat', 'lon'])
         st.write(cor)
         st.map(cor)
