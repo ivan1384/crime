1c921b1ebb2ea0c49d0f3b47b3f5edf384ad9d51
ivan1384 <111645617+ivan1384@users.noreply.github.com>
2022-08-23 12:26:22 +0800

    Create arima_ts.py

---
+++ arima_ts.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+
+def show_arima_page():
+  st.header("Arima output ")
+  
+  
+  HtmlFile = open("prediction_assault.html", 'r', encoding='utf-8')
+  source_code = HtmlFile.read() 
+  print(source_code)
+  components.html(source_code)
+  
+  
+  
