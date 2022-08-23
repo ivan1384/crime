66a31aa19b5e3d4f8b9fe4ab6e1ef81c5e41f1cb
ivan1384 <111645617+ivan1384@users.noreply.github.com>
2022-08-23 12:21:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import streamlit as st
 from predict_alarm import show_alarm_page
 from explore_page import show_explore_page
 from crimeclassifier import show_classifier_page
+from arima_ts import show_arima_page
 
-page = st.sidebar.selectbox("Explore Or Hotspot", ("Hotspot", "Classifier","Explore"))
+page = st.sidebar.selectbox("Explore Or Hotspot", ("Hotspot", "Classifier","Arima","Explore"))
 
 if page == "Hotspot":
     show_alarm_page()
 elif page == "Classifier":
     show_classifier_page()
+elif page == "Arima":
+    show_arima_page()
 else:
     show_explore_page()
