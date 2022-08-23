73595b01e7bdc1353114d3b2c7be2581c0e184dc
ivan1384 <111645617+ivan1384@users.noreply.github.com>
2022-08-23 20:03:16 +0800

    Update arima_ts.py

--- arima_ts.py
+++ arima_ts.py
@@ -5,8 +5,8 @@ import streamlit.components.v1 as components
 def show_arima_page():
   st.header("Arima output ")
   
-  st.subheader("Predicted Assault cases ")
-  HtmlFile = open("prediction_assault.html", 'r', encoding='utf-8')
+  st.subheader("Predicted cases for top 10 crimes")
+  HtmlFile = open("prediction_top10 crimes.html", 'r', encoding='utf-8')
   source_code = HtmlFile.read() 
   print(source_code)
   components.html(source_code,height = 600)
@@ -16,3 +16,12 @@ def show_arima_page():
   source_code2 = HtmlFile2.read() 
   components.html(source_code2,height = 600)
   
+  st.subheader("Predicted Assault cases ")
+  HtmlFile3 = open("prediction_Assault.html", 'r', encoding='utf-8')
+  source_code3 = HtmlFile2.read() 
+  components.html(source_code2,height = 600)
+  
+  st.subheader("Predicted Vehicle Theft cases ")
+  HtmlFile4 = open("prediction_Vehicle.html", 'r', encoding='utf-8')
+  source_code4 = HtmlFile2.read() 
+  components.html(source_code2,height = 600)
